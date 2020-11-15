1a81f9cac3f37ca3b4ab54f41c4c3c7951cb5388
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2020-11-15 19:18:53 +0100

    Python now supports aliased sub-parsers built-in

--- u4pak.py
+++ u4pak.py
@@ -1515,40 +1515,7 @@ def deamonize(stdout='/dev/null', stderr=None, stdin='/dev/null'):
 def main(argv):
 	import argparse
 
-	# from https://gist.github.com/sampsyo/471779
-	class AliasedSubParsersAction(argparse._SubParsersAction):
-
-		class _AliasedPseudoAction(argparse.Action):
-			def __init__(self, name, aliases, help):
-				dest = name
-				if aliases:
-					dest += ' (%s)' % ','.join(aliases)
-				sup = super(AliasedSubParsersAction._AliasedPseudoAction, self)
-				sup.__init__(option_strings=[], dest=dest, help=help)
-
-		def add_parser(self, name, **kwargs):
-			if 'aliases' in kwargs:
-				aliases = kwargs['aliases']
-				del kwargs['aliases']
-			else:
-				aliases = []
-
-			parser = super(AliasedSubParsersAction, self).add_parser(name, **kwargs)
-
-			# Make the aliases work.
-			for alias in aliases:
-				self._name_parser_map[alias] = parser
-			# Make the help text reflect them, first removing old help entry.
-			if 'help' in kwargs:
-				help = kwargs.pop('help')
-				self._choices_actions.pop()
-				pseudo_action = self._AliasedPseudoAction(name, aliases, help)
-				self._choices_actions.append(pseudo_action)
-
-			return parser
-
 	parser = argparse.ArgumentParser(description='unpack, list and mount Unreal Engine 4 .pak archives')
-	parser.register('action', 'parsers', AliasedSubParsersAction)
 	parser.set_defaults(print0=False,verbose=False,check_integrity=False,progress=False,zlib=False,command=None)
 
 	subparsers = parser.add_subparsers(metavar='command')
